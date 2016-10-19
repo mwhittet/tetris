6bffe65ca36326d511a5cb092a2ee0cc55a9d6de
Michael Whittet <5453237+mwhittet@users.noreply.github.com>
2016-10-19 17:17:19 +0100

    Nearly there, looking at scores next.

--- tetris.js
+++ tetris.js
@@ -4,6 +4,20 @@ const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
 context.scale(20, 20);
 
+function arenaSweep() {
+  // debugger;
+  outer: for (let y = arena.length - 1; y > 0; y--) {
+    for (let x = 0; x < arena[y].length; x++) {
+      if (arena[y][x] === 0) {
+        continue outer;
+      }
+    }
+    const row = arena.splice(y, 1)[0].fill(0);
+    arena.unshift(row);
+    y++;
+  }
+}
+
 function collide(arena, player) {
   const [m, o] = [player.matrix, player.pos];
   for (let y = 0; y < m.length; ++y) {
@@ -112,6 +126,7 @@ function playerDrop() {
     player.pos.y--;
     merge(arena, player);
     playerReset();
+    arenaSweep();
   }
   dropCounter = 0;
 }
